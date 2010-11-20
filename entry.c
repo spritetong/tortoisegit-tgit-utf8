db1222883fb3cadd245383f53bad8552e1248fa5
Frank Li <lznuaa@gmail.com>
2010-11-20 15:07:45 +0800

    Export write_entry to checkout one file at gitdll
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- entry.c
+++ entry.c
@@ -177,7 +177,7 @@ close_and_exit:
 	return result;
 }
 
-static int write_entry(struct cache_entry *ce, char *path, const struct checkout *state, int to_tempfile)
+int write_entry(struct cache_entry *ce, char *path, const struct checkout *state, int to_tempfile)
 {
 	unsigned int ce_mode_s_ifmt = ce->ce_mode & S_IFMT;
 	int fd, ret, fstat_done = 0;
