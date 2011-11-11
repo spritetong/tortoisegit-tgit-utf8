a5279c10d0893cbf17e11e3d822a487fd837f2d0
Sven Strickroth <email@cs-ware.de>
2011-11-12 00:49:13 +0100

    do not use pager (also caused crashes in gitdll.dll in some cases)
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- pager.c
+++ pager.c
@@ -48,25 +48,7 @@ static void wait_for_pager_signal(int signo)
 
 const char *git_pager(int stdout_is_tty)
 {
-	const char *pager;
-
-	if (!stdout_is_tty)
-		return NULL;
-
-	pager = getenv("GIT_PAGER");
-	if (!pager) {
-		if (!pager_program)
-			git_config(git_default_config, NULL);
-		pager = pager_program;
-	}
-	if (!pager)
-		pager = getenv("PAGER");
-	if (!pager)
-		pager = DEFAULT_PAGER;
-	else if (!*pager || !strcmp(pager, "cat"))
-		pager = NULL;
-
-	return pager;
+	return NULL;
 }
 
 void setup_pager(void)
