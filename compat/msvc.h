d124f2d13fae0f2f14f4ab063dd7d673f5cb45dc
Sven Strickroth <email@cs-ware.de>
2012-05-22 19:40:14 +0200

    Fixed issue #1195: Problems with tags containing non-ascii chars
    dirent.* taken from msysgit project.
    
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- compat/msvc.h
+++ compat/msvc.h
@@ -62,6 +62,7 @@ int mingw_fstat(int fd, struct stat *buf);
 #define	RUNTIME_PREFIX
 #define	NO_POSIX_ONLY_PROGRAMS
 #define	NO_ST_BLOCKS_IN_STRUCT_STAT
+#define NO_D_INO_IN_DIRENT
 #define	NO_NSEC 
 #ifndef USE_WIN32_MMAP
 #define	USE_WIN32_MMAP
